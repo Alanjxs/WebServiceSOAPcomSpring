64064aa1b80961ffeb5f31a200c8672bb232dc5f
alanjxs@gmail.com <alanjxs@gmail.com>
2023-02-17 09:54:30 -0300

    Criando o endpoint e configurando o WebServiceConfig

---
+++ src/main/java/com/example/soap/webservice/customersadministration/soap/CustomerDetailEndPoint.java
@@ -0,0 +1,28 @@
+package com.example.soap.webservice.customersadministration.soap;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import br.com.klayrocha.CustomerDetail;
+import br.com.klayrocha.GetCustomerDetailRequest;
+import br.com.klayrocha.GetCustomerDetailResponse;
+
+@Endpoint
+public class CustomerDetailEndPoint {
+	
+	
+	@PayloadRoot(namespace = "http://klayrocha.com.br", localPart = "GetCustomerDetailRequest")
+	@ResponsePayload
+	public GetCustomerDetailResponse processCustomerDetailRequest(@RequestPayload GetCustomerDetailRequest req) {
+		GetCustomerDetailResponse response = new GetCustomerDetailResponse();
+		CustomerDetail customerDetail = new CustomerDetail();
+		customerDetail.setId(1);
+		customerDetail.setName("Bob");
+		customerDetail.setPhone("99357918");
+		customerDetail.setEmail("bob@gmail.com");
+		response.setCustomerDetail(customerDetail);
+		return response;
+	}
+}
